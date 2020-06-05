81675e5cd8124b7705cff2da3730c78674a6a3bb
L. M. Riza Rizky <riza.kepler@gmail.com>
2020-06-05 14:43:29 +0800

    Added third assignment project

---
+++ server-side-development-node/assignment-3/routes/promoRouter.js
@@ -0,0 +1,94 @@
+const express = require("express");
+
+const Promotions = require("../models/promotions");
+
+const leaderRouter = express.Router();
+
+leaderRouter
+  .route("/")
+  .get((req, res, next) => {
+    Promotions.find()
+      .then((promotions) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(promotions);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  })
+  .post((req, res, next) => {
+    Promotions.create(req.body)
+      .then((promotion) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(promotion);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  })
+  .put((req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /promotions");
+  })
+  .delete((req, res, next) => {
+    Promotions.remove().then((resp) => {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.json(resp);
+    });
+  });
+
+leaderRouter
+  .route("/:promoId")
+  .get((req, res, next) => {
+    Promotions.findById(req.params.promoId)
+      .then(
+        (dish) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(dish);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .post((req, res, next) => {
+    res.statusCode = 403;
+    res.end(
+      "POST operation not supported on /promotions/" + req.params.promoId
+    );
+  })
+  .put((req, res, next) => {
+    Promotions.findByIdAndUpdate(
+      req.params.promoId,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    )
+      .then(
+        (dish) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(dish);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .delete((req, res, next) => {
+    Promotions.findByIdAndRemove(req.params.promoId)
+      .then(
+        (resp) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(resp);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  });
+
+module.exports = leaderRouter;
